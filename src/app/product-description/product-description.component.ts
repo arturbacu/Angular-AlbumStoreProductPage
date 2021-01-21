0d418db908f7ea9150003a1f4863ab091d27bc45
Artur Bacu <artur.bacu@emc.com>
2021-01-21 11:54:31 -0500

    Part 4 complete

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,5 +1,8 @@
-import { ProductService } from '../product.service';
 import { Component, OnInit } from '@angular/core';
+
+import { Album } from '../album';
+import { ProductService } from '../product.service';
+
 
 @Component({
   selector: 'app-product-description',
@@ -8,7 +11,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDescriptionComponent implements OnInit {
 
-  albumInfo;
+  albumInfo: Album;
 
   constructor(private _productService: ProductService) { }
 
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,6 +3,8 @@ import { Http, Response } from '@angular/http';
 import { Injectable } from '@angular/core';
 import 'rxjs/add/operator/map';
 
+import { Album } from './album';
+
 @Injectable()
 export class ProductService {
 
@@ -11,7 +13,7 @@ export class ProductService {
   constructor(private _http: Http) { }
 
   getAlbum(id: number): Observable<Album> {
-    return this._http.get(this._albumUrl).map(response => response.json());
+    return this._http.get(this._albumUrl).map(response => <Album>response.json());
   }
 
 }
